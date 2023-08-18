19b675b694b1aed6230d21705fc3e9a99fa6e08e
Federico E. Benelli <fedebenelli@outlook.com>
2023-08-18 16:10:58 -0300

    feat(Px envelopes): Main logic to calculate the system of Px envelopes

--- app/main.f90
+++ app/main.f90
@@ -92,4 +92,121 @@ contains
       end if
       ! =====================================================================
    end subroutine
+
+   subroutine px_envelopes
+      !! Calculation of Px envelopes at selected temperature.
+      use inj_envelopes, only: F_injection, full_newton, z_injection, &
+                               T_inj => T, injection_envelope, z_0, injection_case, &
+                               injelope, funit_output
+      use envelopes, only: envelope, k_wilson, p_wilson
+      use linalg, only: interpol
+
+      real(pr), allocatable :: X(:), F(:), F2(:), dF(:, :), df_num(:, :)
+      real(pr) :: alpha, S
+      integer :: ns, i, iters, idx, ti
+      integer, allocatable :: i_inj(:)
+      real(pr), allocatable :: ts_envel(:), ts(:)
+      real(pr) :: t_tol = 2
+
+      type(injelope) :: bub_envels, dew_envels
+      allocate (X(nc + 2), F(nc + 2), dF(nc + 2, nc + 2), df_num(nc + 2, nc + 2), F2(nc + 2))
+
+      ! ======================================================================
+      !  Setup system
+      ! ----------------------------------------------------------------------
+      alpha = 0.0
+      z_injection = z_injection/sum(z_injection)
+      ns = nc + 2
+      open (newunit=funit_output, file="px.dat")
+      ! ======================================================================
+
+      ! ======================================================================
+      !  Bubble envelope
+      ! ----------------------------------------------------------------------
+      print *, "Running Bubble"
+      bubble: block
+         real(pr) :: pold
+         pold = 0
+         ts_envel = pack(bub_env%t, mask=abs(bub_env%t - t_inj) < t_tol)
+         do i = 1, size(ts_envel)
+            idx = findloc(bub_env%t, value=ts_envel(i), dim=1)
+            print *, ts_envel(i)
+
+            p = interpol( &
+                bub_env%t(idx), bub_env%t(idx + 1), &
+                bub_env%p(idx), bub_env%p(idx + 1), &
+                t_inj)
+
+            if (abs(p - pold) < 5) cycle
+            pold = p
+
+            k = exp(interpol( &
+                    bub_env%t(idx), bub_env%t(idx + 1), &
+                    bub_env%logk(idx, :), bub_env%logk(idx + 1, :), &
+                    t_inj))
+
+            X(1:nc) = log(K)
+            X(nc + 1) = log(P)
+            X(nc + 2) = alpha
+
+            call injection_envelope(X, ns, 0.01_pr, bub_envels)
+         end do
+      end block bubble
+      ! ======================================================================
+
+      ! ======================================================================
+      !  Dew envelope
+      ! ----------------------------------------------------------------------
+      print *, "Running Dew"
+      dew: block
+         real(pr) :: pold
+         pold = 0
+         ts_envel = pack(dew_env%t, mask=abs(dew_env%t - t_inj) < t_tol)
+         do i = 1, size(ts_envel)
+            idx = findloc(dew_env%t, value=ts_envel(i), dim=1)
+
+            alpha = 0
+
+            p = interpol( &
+                dew_env%t(idx), dew_env%t(idx + 1), &
+                dew_env%p(idx), dew_env%p(idx + 1), &
+                t_inj)
+
+            if (abs(p - pold) < 5) cycle
+            pold = p
+            print *, ts_envel(i), p
+
+            k = exp(interpol( &
+                    dew_env%t(idx), dew_env%t(idx + 1), &
+                    dew_env%logk(idx, :), dew_env%logk(idx + 1, :), &
+                    t_inj))
+
+            X(1:nc) = log(K)
+            X(nc + 1) = log(P)
+            X(nc + 2) = alpha
+
+            call injection_envelope(X, ns, 0.01_pr, dew_envels)
+         end do
+      end block dew
+      ! ======================================================================
+
+      ! ======================================================================
+      !  Look for crossings
+      ! ----------------------------------------------------------------------
+      check_crossings: block
+         use linalg, only: point, intersection
+         type(point), allocatable :: inter(:)
+         inter = intersection( &
+                 dew_envels%alpha, dew_envels%p, &
+                 bub_envels%alpha, bub_envels%p &
+                 )
+
+         do i = 1, size(inter)
+            print *, inter(i)
+         end do
+      end block check_crossings
+      ! ======================================================================
+      close (funit_output)
+      print *, "END"
+   end subroutine
 end program main
