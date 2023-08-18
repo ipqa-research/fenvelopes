d60bf97963054cb0280cbcf92e8b597a600c2607
Federico E. Benelli <fedebenelli@outlook.com>
2023-08-18 16:13:45 -0300

    feat(intersect): Function to get a self cross

--- src/linalg.f90
+++ src/linalg.f90
@@ -24,6 +24,44 @@ contains
 
       x = b_lapack
    end function solve_system
+   function intersect_one_line(lx, ly) result(intersections)
+      real(pr), intent(in) :: lx(:), ly(:)
+      type(point), allocatable :: intersections(:)
+      character(len=*), parameter :: fmt="(*(G0,:,', '))"
+
+      real(pr) :: s, t
+      integer :: i, j
+
+      real(pr) :: x, y, xold=9999, yold=9999
+
+      allocate(intersections(0))
+      line1: do i=2, size(lx)-1
+         line2: do j=i+15, size(lx)
+            associate(x1 => lx(i-1), x2 => lx(i), x3 => lx(j), x4 => lx(j-1), &
+                      y1 => ly(i-1), y2 => ly(i), y3 => ly(j), y4 => ly(j-1))
+            call intersects(x1, x2, x3, x4, y1, y2, y3, y4, s, t)
+            if (0 <= s .and. s <= 1 .and. 0 <= t .and. t <= 1) then
+            
+               x = s*(x2 - x1) + x1
+               y = s*(y2 - y1) + y1
+
+            if (abs(x - xold) > 1 .and. abs(y - yold) > 1) then
+               print *, "CROSS"
+               print *, i, j, x, y
+               write(*, fmt) "x1, y1 = ", x1, y1
+               write(*, fmt) "x2, y2 = ", x2, y2
+               write(*, fmt) "x3, y3 = ", x3, y3
+               write(*, fmt) "x4, y4 = ", x4, y4
+               xold = x
+               yold = y
+               intersections = [intersections, point(x, y, i, j)]
+            end if
+            end if
+            end associate
+         end do line2
+      end do line1
+   end function
+
    subroutine intersects(x1, x2, x3, x4, y1, y2, y3, y4, s, t)
       real(pr), intent(in) :: x1, x2, x3, x4, y1, y2, y3, y4
       real(pr), intent(out) :: s, t
