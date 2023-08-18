03f23b1e00c6c573de5667dcd8e3d58b6fe8a2ee
Federico E. Benelli <fedebenelli@outlook.com>
2023-08-18 16:12:01 -0300

    feat(linalg): interpolation function

--- src/linalg.f90
+++ src/linalg.f90
@@ -24,4 +24,13 @@ contains
 
       x = b_lapack
    end function solve_system
+   elemental function interpol(x1, x2, y1, y2, x_obj) result(y)
+      real(pr), intent(in) :: x1
+      real(pr), intent(in) :: x2
+      real(pr), intent(in) :: y1
+      real(pr), intent(in) :: y2
+      real(pr), intent(in) :: x_obj
+      real(pr) :: y
+      y = (y2 - y1)/(x2 - x1) * (x_obj - x1) + y1
+   end function
 end module linalg
