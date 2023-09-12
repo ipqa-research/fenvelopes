8e40597536ff6ee809c83a6896610c304d15a79d
Federico E. Benelli <fedebenelli@outlook.com>
2023-09-12 14:45:48 -0300

    interpol

--- src/linalg.f90
+++ src/linalg.f90
@@ -29,7 +29,7 @@ contains
          subroutine dgesv(n, nrhs, a, lda, ipiv, b, ldb, info)
             integer :: n
             integer :: nrhs
-            real(8) :: a(n,n)
+            real(8) :: a(n, n)
             integer :: lda
             integer :: ipiv(n)
             real(8) :: b(n)
@@ -57,24 +57,29 @@ contains
       real(pr) :: s, t
       integer :: i, j
 
-      real(pr) :: x, y, xold=9999, yold=9999
+      real(pr) :: x, y, xold = 9999, yold = 9999
 
-      allocate(intersections(0))
+      allocate (intersections(0))
 
-      line1: do i=2, size(l1_x)
-         line2: do j=2, size(l2_x)
-            associate(x1 => l1_x(i-1), x2 => l1_x(i), x3 => l2_x(j-1), x4 => l2_x(j), &
-                      y1 => l1_y(i-1), y2 => l1_y(i), y3 => l2_y(j-1), y4 => l2_y(j))
+      line1: do i = 2, size(l1_x)
+         line2: do j = 2, size(l2_x)
+            associate ( &
+               x1 => l1_x(i - 1), x2 => l1_x(i), &
+               x3 => l2_x(j - 1), x4 => l2_x(j), &
+               y1 => l1_y(i - 1), y2 => l1_y(i), &
+               y3 => l2_y(j - 1), y4 => l2_y(j))
                call intersects(x1, x2, x3, x4, y1, y2, y3, y4, s, t)
 
                if (0 <= s .and. s <= 1 .and. 0 <= t .and. t <= 1) then
-                  x = s * (x2-x1) + x1
-                  y = s * (y2-y1) + y1
+                  x = s*(x2 - x1) + x1
+                  y = s*(y2 - y1) + y1
 
-                  if (abs((x - xold)) > 1e-5_pr .and. abs((y - yold)) > 1e-5_pr) then
+                  if ( &
+                     abs((x - xold)) > 1e-5_pr .and. abs((y - yold)) > 1e-5_pr &
+                     ) then
                      xold = x
                      yold = y
-                    intersections = [intersections, point(x, y, i, j)]
+                     intersections = [intersections, point(x, y, i, j)]
                      exit line2
                   end if
 
@@ -83,34 +88,36 @@ contains
          end do line2
       end do line1
    end function
-   
+
    function intersect_one_line(lx, ly) result(intersections)
       real(pr), intent(in) :: lx(:), ly(:)
       type(point), allocatable :: intersections(:)
-      character(len=*), parameter :: fmt="(*(G0,:,', '))"
 
       real(pr) :: s, t
       integer :: i, j
 
-      real(pr) :: x, y, xold=9999, yold=9999
+      real(pr) :: x, y, xold = 9999, yold = 9999
 
-      allocate(intersections(0))
-      line1: do i=2, size(lx)-1
-         line2: do j=i+15, size(lx)
-            associate(x1 => lx(i-1), x2 => lx(i), x3 => lx(j), x4 => lx(j-1), &
-                      y1 => ly(i-1), y2 => ly(i), y3 => ly(j), y4 => ly(j-1))
-            
-            call intersects(x1, x2, x3, x4, y1, y2, y3, y4, s, t)
-            if (0 <= s .and. s <= 1 .and. 0 <= t .and. t <= 1) then
-               x = s*(x2 - x1) + x1
-               y = s*(y2 - y1) + y1
-               if (abs(x - xold) > 1 .and. abs(y - yold) > 1) then
-                  xold = x
-                  yold = y
-                  ! Use earliest point for the "other" line
-                  intersections = [intersections, point(x, y, i, j-1)]
+      allocate (intersections(0))
+      line1: do i = 2, size(lx) - 1
+         line2: do j = i + 15, size(lx)
+            associate ( &
+               x1 => lx(i - 1), x2 => lx(i), &
+               x3 => lx(j), x4 => lx(j - 1), &
+               y1 => ly(i - 1), y2 => ly(i), &
+               y3 => ly(j), y4 => ly(j - 1))
+
+               call intersects(x1, x2, x3, x4, y1, y2, y3, y4, s, t)
+               if (0 <= s .and. s <= 1 .and. 0 <= t .and. t <= 1) then
+                  x = s*(x2 - x1) + x1
+                  y = s*(y2 - y1) + y1
+                  if (abs(x - xold) > 1 .and. abs(y - yold) > 1) then
+                     xold = x
+                     yold = y
+                     ! Use earliest point for the "other" line
+                     intersections = [intersections, point(x, y, i, j - 1)]
+                  end if
                end if
-            end if
             end associate
          end do line2
       end do line1
@@ -120,11 +127,11 @@ contains
       real(pr), intent(in) :: x1, x2, x3, x4, y1, y2, y3, y4
       real(pr), intent(out) :: s, t
 
-      real(pr) :: A(2,2), b(2), tmp
+      real(pr) :: A(2, 2), b(2), tmp
 
-      A(1, :) = [x2-x1, x3-x4]
-      A(2, :) = [y2-y1, y3-y4]
-      b = [x3-x1, y3-y1]
+      A(1, :) = [x2 - x1, x3 - x4]
+      A(2, :) = [y2 - y1, y3 - y4]
+      b = [x3 - x1, y3 - y1]
 
       b = solve_system(a, b)
       s = b(1)
@@ -132,12 +139,40 @@ contains
    end subroutine
 
    elemental function interpol(x1, x2, y1, y2, x_obj) result(y)
-      real(pr), intent(in) :: x1
-      real(pr), intent(in) :: x2
-      real(pr), intent(in) :: y1
-      real(pr), intent(in) :: y2
-      real(pr), intent(in) :: x_obj
-      real(pr) :: y
-      y = (y2 - y1)/(x2 - x1) * (x_obj - x1) + y1
+      !! Linear interpolation.
+      !!
+      !! Calculates the linear interpolation between two points at a desired
+      !! x value with the equation:
+      !! \[ 
+      !!    y = \frac{y_2 - y_1}{x_2 - x_1} \cdot (x_{obj})  - x_1 + y_1
+      !! \]
+      !! 
+      !! Since this function is defined as `elemental` it will also interpolate
+      !! a set of vectors.
+      !!
+      !! Examples of usage:
+      !!
+      !! ```fortran
+      !! x1 = 2
+      !! x2 = 5
+      !! y1 = 2
+      !! y2 = 9
+      !! y = interpol(x1, x2, y1, y2, 2.3)
+      !! ```
+      !!
+      !! ```fortran
+      !! x1 = 2
+      !! x2 = 5
+      !! y1 = [2, 6]
+      !! y2 = [9, 15]
+      !! y = interpol(x1, x2, y1, y2, 2.3)
+      !! ```
+      real(pr), intent(in) :: x1 !! First point x value
+      real(pr), intent(in) :: x2 !! Second point x value
+      real(pr), intent(in) :: y1 !! First point y value 
+      real(pr), intent(in) :: y2 !! Second point y value
+      real(pr), intent(in) :: x_obj !! Desired x value to interpolate
+      real(pr) :: y !! y value at `x_obj`
+      y = (y2 - y1)/(x2 - x1)*(x_obj - x1) + y1
    end function
 end module linalg
