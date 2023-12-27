e37c048b3773fb8b1240229411ff1c2b0a98a54d
Federico E. Benelli <fedebenelli@outlook.com>
2023-12-27 14:37:34 -0300

    details on constants

--- src/new/constants.f90
+++ src/new/constants.f90
@@ -2,9 +2,8 @@ module constants
    use iso_fortran_env, only: real32, real64, real128
    implicit none
 
-   integer, parameter :: pr = real64
-   real(pr), parameter :: R = 0.08314472
-   character(len=254) :: database_path = "database/"
-   character(len=254) :: ouput_path = "fenvelopes_output/"
-   character(len=1) :: path_sep = "/"
+   integer, parameter :: pr = real64 !! Precision
+   real(pr), parameter :: R = 0.08314472 !! R constant []
+   character(len=254) :: ouput_path = "fenvelopes_output/" !! Output path
+   character(len=1) :: path_sep = "/" !! Path separator
 end module constants
