d6be2a8add636674fbfdcc5d852bf7856b660b76
Federico E. Benelli <fedebenelli@outlook.com>
2024-01-05 09:37:59 -0300

    feat(io): Encapsulating all the possible cli IO in a module

---
+++ src/io/cli.f90
@@ -0,0 +1,64 @@
+module fenvelopes_cli
+   !! Module for setting up command line interfaces for different programs
+   use flap, only: command_line_interface
+contains
+   subroutine setup_cli_envelopes(cli)
+      !! Setup CLI subroutine for phase envelopes calculation
+      !!
+      !! Setup the Command-Line-Interface processor
+      type(command_line_interface), intent(in out) :: cli
+      integer :: cli_error
+
+      call cli%init(progname="envelopes", description="Phase Envelopes")
+      call cli%add( &
+         switch="--infile", &
+         switch_ab="-i", &
+         help="Input file", &
+         error=cli_error, &
+         required=.true.)
+
+      call cli%add( &
+         switch="--injection", &
+         switch_ab="-px", &
+         help="Trace Px lines", &
+         error=cli_error, &
+         required=.false.)
+      call cli%parse(error=cli_error)
+
+      if (cli_error /= 0) stop
+   end subroutine
+
+   subroutine setup_cli_isotherms(cli)
+      !! Setup CLI subroutine for isotherms calculations
+      !!
+      !! Setup the Command-Line-Interface processor
+      type(command_line_interface), intent(in out) :: cli
+      integer :: cli_error
+   end subroutine
+   
+   subroutine setup_cli_flash(cli)
+      !! Setup CLI subroutine for flash calculations
+      !!
+      !! Setup the Command-Line-Interface processor
+      type(command_line_interface), intent(in out) :: cli
+      integer :: cli_error
+   end subroutine
+   
+   subroutine setup_cli_bubble(cli)
+      !! Setup CLI subroutine for bubble points
+      !!
+      !! Setup the Command-Line-Interface processor
+      type(command_line_interface), intent(in out) :: cli
+      integer :: cli_error
+
+      call cli%init(progname="bubble_point", description="Bubble point")
+      call cli%add( &
+         switch="--infile", &
+         switch_ab="-i", &
+         help="Input file", &
+         error=cli_error, &
+         required=.true.)
+
+      if (cli_error /= 0) stop
+   end subroutine
+end module
